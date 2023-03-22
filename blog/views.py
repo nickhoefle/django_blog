b8f1ec3a2f9ba890036d83671c241fa1b7ec7248
Nick <nickhoefle@gmail.com>
2023-03-21 22:01:19 -0500

    created Post class and registered it with the admin page.

--- blog/views.py
+++ blog/views.py
@@ -1,24 +1,10 @@
 from django.shortcuts import render
-
-posts = [
-    {
-        'author': 'NickHoefle',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'March 18, 2023'
-    },
-    {
-        'author': 'Jane Doe',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'March 16, 2023'
-    }
-]
+from .models import Post
 
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
